7cf95a63fdf5ca5b447e10ec50bbcfa24e310137
Ryan Jones (AZURE RESOURCE MANAGER) <ryjones@microsoft.com>
2017-05-10 20:44:52 -0700

    adding Azure Monitor

---
+++ Act4.AzureMonitor/Chapter11.AzureMonitor.ps1
@@ -0,0 +1,3 @@
+﻿New-AzureRmResourceGroup -Name templateEnhancements -Location eastus2euap -Force
+
+New-AzureRmResourceGroupDeployment -ResourceGroupName templateEnhancements -TemplateUri https://raw.githubusercontent.com/rjmax/Build2017/master/Act1.TemplateEnhancements/Chapter01.Functions.json
