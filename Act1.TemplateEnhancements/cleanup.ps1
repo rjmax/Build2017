56794921cb5c26603927b93b36de9ba82f1d709a
Ryan Jones (AZURE RESOURCE MANAGER) <ryjones@microsoft.com>
2017-05-10 20:53:34 -0700

    fixing RG

--- Act1.TemplateEnhancements/cleanup.ps1
+++ Act1.TemplateEnhancements/cleanup.ps1
@@ -4,3 +4,4 @@ Remove-AzureRmResourceGroup -Name serialCopies -Force
 Remove-AzureRmResourceGroup -Name crossResourceGroupDeployment -Force
 Remove-AzureRmResourceGroup -Name conditionalResourcesSsh -Force
 Remove-AzureRmResourceGroup -Name conditionalResourcesPassword -Force
+Remove-AzureRmResourceGroup -Name azureMonitor -Force
