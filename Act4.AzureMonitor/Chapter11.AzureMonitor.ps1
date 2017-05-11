895233c667d9bcc825337c45833776e9f0a608d6
Ryan Jones (AZURE RESOURCE MANAGER) <ryjones@microsoft.com>
2017-05-11 09:01:49 -0700

    naming update for azureMonitor

--- Act4.AzureMonitor/Chapter11.AzureMonitor.ps1
+++ Act4.AzureMonitor/Chapter11.AzureMonitor.ps1
@@ -1,3 +1,3 @@
 ﻿New-AzureRmResourceGroup -Name azureMonitor -Location westus -Force
 
-New-AzureRmResourceGroupDeployment -ResourceGroupName azureMonitor -TemplateUri https://raw.githubusercontent.com/rjmax/Build2017/master/Act4.AzureMonitor/azureMonitor.json
+New-AzureRmResourceGroupDeployment -ResourceGroupName azureMonitor -TemplateUri https://raw.githubusercontent.com/rjmax/Build2017/master/Act4.AzureMonitor/Chapter11.azureMonitor.json
